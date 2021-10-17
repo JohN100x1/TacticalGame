2d13764fb877438964c329ca77abf3d8e28d6bde
John Kong <JohN100x1@hotmail.co.uk>
2021-10-17 11:07:05 +0100

    v0.2 - transition to battle screen

--- app/src/main/java/com/example/tacticalgame/Fragments/StartFragment.java
+++ app/src/main/java/com/example/tacticalgame/Fragments/StartFragment.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,10 +18,22 @@ import android.widget.TextView;
 import com.example.tacticalgame.R;
 
 public class StartFragment extends Fragment {
+
+    private TextView startButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_start, container, false);
+        startButton = view.findViewById(R.id.start_btn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_frame, new BattleFragment());
+                transaction.commit();
+            }
+        });
         return view;
     }
     @Override
